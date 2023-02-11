473ca8e0fc240ed02b596d0eaedff0d02719817d
Matěj Bednář <99074840+Mxthesss@users.noreply.github.com>
2023-02-11 23:00:11 +0100

    1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+game "gta5"
+
+author "Mxthess"
+
+description "MX-NoMinimap"
+
+version "1.0.0"
+
+client_script "mx_client.lua"
---
+++ mx_client.lua
@@ -0,0 +1,10 @@
+Citizen.CreateThread(function()
+    while trye do
+      wait(1)
+      if not displayRadar then
+        DisplayRadar(false)
+        end
+      end
+    end)
+    
+print('^5Made By Mxthess^7: ^1'..GetCurrentResourceName()..'^7 started ^2successfully^7...') 
